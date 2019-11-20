05d85851561287dc4cef6491bf626a3607d5f8e3
Jorge Martinez Barca <jorge.martinez.barca@gmail.com>
2019-11-20 22:30:26 +0100

    Refactorizando validacions.

--- js/validations.js
+++ js/validations.js
@@ -1,47 +1,23 @@
 window.onload = eventsLoad;
 
+/***************************** EVENTOS ********************************/
 function eventsLoad() {
-    document.getElementById("btnRegister").addEventListener("click", validateRegistration, true);
-}
-
-function validateRegistration(event) {
-    event.preventDefault();
-
-    var name = document.getElementById("name");
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
-    var repeatPassword = document.getElementById("repeatPassword");
-    var bornDate = document.getElementById("bornDate");
-    var agree = document.getElementById("agree");
-
-    //si todo valida, enviamos petición de buscar usuario
-    if (validateName(name) && validateMail(email) &&
-        validatePassword(password) && validatePassword(repeatPassword) &&
-        validateRepeatPassword(password, repeatPassword) && validateBornDate(bornDate) &&
-        validateTerms(agree)) {
-
-        if (confirm("Todo validado correctamente.")) {
-
-            var newUser = new User(name,email,password,bornDate);
-
-            /**
-            newUser.name = name;
-            newUser.email = email;
-            newUser.password = password;
-            newUser.bornDate = bornDate;
-             */
-
-
-            alert(newUser.toString);
-
-            //document.getElementById("register-validation").submit();
-
-        }
-
-
+    console.log(window.location.href);
+    switch (window.location.href) {
+        case "http://127.0.0.1:5500/login.html":
+            checkCookie();
+            document.getElementById("btnLogin").addEventListener("click", validateLogin, true);
+            break;
+        case "http://127.0.0.1:5500/register.html":
+            document.getElementById("btnRegister").addEventListener("click", validateRegistration, true);
+            break;
+        default:
+            alert("NO coincide ningunha url");
+            break;
     }
 }
 
+/***************************** INPUTS ********************************/
 function validateName(element) {
     var string = element.value;
     var alert = document.getElementById("alert");
@@ -99,17 +75,32 @@ function validatePassword(element) {
     var string = element.value;
     var alert = document.getElementById("alert");
     var alertMessage = document.getElementById("alertMessage");
+    var message = "";
 
     if (string == "" || string == null) {
         alert.style.display = "inline";
         element.classList.add("error");
-        alertMessage.innerHTML = "Las contraseñas no pueden ir vacías."
+
+        if (element.id == "repeatPassword") {
+            message = "Las contraseñas no coinciden.";
+        } else {
+            message = "La contraseña no puede ir vacía.";
+        }
+
+        alertMessage.innerHTML = message;
         element.focus();
         return false;
 
     } else if (string.length < 5) {
         alert.style.display = "inline";
-        alertMessage.innerHTML = "Las contraseñas deben tener un minímo de 5 carácteres.";
+
+        if (element.id == "repeatPassword") {
+            message = "Las contraseñas no coinciden.";
+        } else {
+            message = "La contraseña debe tener un minímo de 5 carácteres.";
+        }
+
+        alertMessage.innerHTML = message;
         element.focus();
         element.classList.add("error");
         //element.className = "error";
@@ -147,7 +138,6 @@ function validateRepeatPassword(element, elementCompare) {
     }
 
 }
-
 
 function validateBornDate(element) {
 
@@ -188,6 +178,15 @@ function validateBornDate(element) {
 
 }
 
+function cleanErrores(listElements) {
+    //limpiamos algún posible error de otra parte del formulario
+    var errores = listElements;
+    for (var i = 0; i < errores.length; i++) {
+        errores[i].classList.remove("error");
+    }
+}
+
+/***************************** CHEKS ********************************/
 function validateTerms(element) {
 
     var alert = document.getElementById("alert");
@@ -209,6 +208,110 @@ function validateTerms(element) {
         element.focus();
         return false;
 
+    }
+}
+
+function checkRemember(element) {
+    var isChecked = element.checked;
+
+    if (isChecked) {
+        return true;
+
+    } else {
+        return false;
+    }
+}
+
+/**************************************
+ *********** VALIDAR LOGIN ************
+ **************************************/
+function validateLogin(event) {
+    event.preventDefault();
+
+    var email = document.getElementById("emailLogin");
+    var password = document.getElementById("passwordLogin");
+
+    var check = document.getElementById("remember");
+
+    if (checkRemember(check)) {
+        document.cookie = "email = " + email.value;
+    } else {
+        document.cookie = "email = ";
+    }
+
+
+    //si todo valida, enviamos petición de buscar usuario
+    if (validateMail(email) && validatePassword(password)) {
+
+        cleanErrores(document.getElementsByTagName("input"));
+
+        if (confirm("Todo validado correctamente.")) {
+
+            alert("Petición de usuario: \n email: " + email.value + " \n password: " + password.value);
+            //document.getElementById("register-validation").submit();        
+        }
+
+    }
+}
+
+/**************************************
+ ********* VALIDAR REGISTRO ***********
+ **************************************/
+function validateRegistration(event) {
+    event.preventDefault();
+
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    var repeatPassword = document.getElementById("repeatPassword");
+    var bornDate = document.getElementById("bornDate");
+    var agree = document.getElementById("agree");
+
+    //si todo valida, enviamos petición de buscar usuario
+    if (validateName(name) && validateMail(email) &&
+        validatePassword(password) && validatePassword(repeatPassword) &&
+        validateRepeatPassword(password, repeatPassword) && validateBornDate(bornDate) &&
+        validateTerms(agree)) {
+
+        if (confirm("Todo validado correctamente.")) {
+
+            var newUser = new User(name, email, password, bornDate);
+
+            /**
+            newUser.name = name;
+            newUser.email = email;
+            newUser.password = password;
+            newUser.bornDate = bornDate;
+             */
+
+
+            alert(newUser.toString);
+
+            //document.getElementById("register-validation").submit();
+
+        }
+
+
+    }
+}
+
+/**************************************
+ ************** COOKIES ***************
+ **************************************/
+function getCookie() {
+    //Esta función la usamos para recuperar la cookie
+    var cookie = document.cookie;
+    var resultado = cookie.split('=');
+    return resultado[1];
+}
+
+function checkCookie() {
+    var mail = getCookie("email");
+
+    if (mail === undefined) {
+        document.getElementById("emailLogin").value = "";
+    } else {
+        document.getElementById("emailLogin").value = mail;
     }
 
 }
